a945f91d15a38df0999813fb80b640f94026c0a3
Julio Betta <juliobetta@gmail.com>
2016-03-18 16:34:41 -0300

    submit a new post

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import { API_URL, API_KEY } from '../constants/common';
 
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 
 
 export function fetchPosts() {
@@ -11,6 +12,17 @@ export function fetchPosts() {
 
   return {
     type:    FETCH_POSTS,
+    payload: request
+  };
+}
+
+
+export function createPost(props) {
+  const url     = `${API_URL}/posts?key=${API_KEY}`;
+  const request = axios.post(url, props);
+
+  return {
+    type:    CREATE_POST,
     payload: request
   };
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import PostsReducer from './reducer-posts';
+import { reducer as formReducer } from 'redux-form';
 
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form:  formReducer
 });
 
 
