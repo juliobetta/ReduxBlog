aef60fd1176f5193eba68c1c8d0a990205f78526
Julio Betta <juliobetta@gmail.com>
2016-03-26 20:41:10 -0300

    use classnames library on Notification component

--- src/components/elements/notification.js
+++ src/components/elements/notification.js
@@ -1,16 +1,17 @@
 import React       from 'react';
+import classNames  from 'classnames';
 import * as Status from '../../constants/alert-status';
 
 
 export default ({ messages, status = Status.INFO }) => {
-  const classNames = ['alert', 'alert-dismissible', status];
+  const classes = classNames(['alert', 'alert-dismissible', status]);
 
   if(!messages.length) {
     return <span></span>;
   }
 
   return (
-    <div className={classNames.join(' ')} role="alert">
+    <div className={classes} role="alert">
       {messages.map((message, i) => <p key={i}>{message}</p>)}
     </div>
   );
